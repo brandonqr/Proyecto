8679997633c3187b8e6e4231f3e70843dbdcafed
Puta <marcxargay@gmail.com>
2017-03-24 20:09:59 +0100

    primer commit

--- php/login.php
+++ php/login.php
@@ -1,7 +1,7 @@
 <?php
 	include_once('model.php');
-	//$connBd = new baseDatos('daw.institutmontilivi.cat','pizzeria', 'p1zz3r14','pizzeria');
-	$connBd = new baseDatos('localhost','root', 'root','pizzeria');
+	$connBd = new baseDatos('daw.institutmontilivi.cat','pizzeria', 'p1zz3r14','pizzeria');
+	//$connBd = new baseDatos('localhost','root', 'root','pizzeria');
 	$usuario = $_GET['usuario'];
 	$contrasena = $_GET['contrasena'];
 	$existeUsuario = $connBd->comprobarSiExisteUsuario($usuario, $contrasena);
